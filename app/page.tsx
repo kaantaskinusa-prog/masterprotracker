fe84bcfcc4a704c7b4a82df5f1f8601c6c60feac
Kaan Taskin <kaantaskinusa@gmail.com>
2026-07-13 21:23:54 -0400

    use client eklendi

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
 'use client';
+
+import { useState } from 'react';
+import Calculator from './Calculator'; 
+import LoginPage from './LoginPage';
+
+export default function Home() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  if (!isAuthenticated) {
+    return <LoginPage onLogin={() => setIsAuthenticated(true)} />;
+  }
+
+  return <Calculator />;
+}
+'use client';
 import { useState } from 'react';
 import Calculator from './Calculator'; 
 import LoginPage from './LoginPage';
